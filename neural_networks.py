9f96d088931ff039e32ba4a51ded776dc9969713
Beta-JP <jpmash2003@gmail.com>
2025-05-26 20:06:23 +0200

    L2-NN is learning correktly now

--- neural_networks.py
+++ neural_networks.py
@@ -14,14 +14,14 @@ def build_standard_nn(hidden_layer, neurons_per_layer, activation_function, weig
     model.add(tf.keras.layers.Dense(units=3, activation=None, kernel_initializer=weight_initializer(), name="output"))
     return model
 
-def build_l2_regularized_nn(hidden_layer, neurons_per_layer, activation_function, weight_initializer, l2_factor=0.1):
+def build_l2_regularized_nn(hidden_layer, neurons_per_layer, activation_function, weight_initializer, l2_factor=1e-4):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.InputLayer(input_shape=(1,), name="input"))
     for i in range(hidden_layer):
         model.add(tf.keras.layers.Dense(
             units=neurons_per_layer,
             activation=activation_function,
-            #kernel_initializer=weight_initializer(), // NOTE: UNCOMMENT THIS LINE IF IT ISN'T -DEBUG PURPOSES
+            kernel_initializer=weight_initializer(),
             kernel_regularizer=regularizers.l2(l2_factor),
             name=f"hidden_{i}"))
     model.add(tf.keras.layers.Dense(units=3, activation=None, kernel_initializer=weight_initializer(), name="output"))
